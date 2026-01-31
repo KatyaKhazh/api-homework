d38968a987a5478c1e43f579ce842cfe4694f7cd
Katya test <eakhazhinova@edu.hse.ru>
2026-01-31 12:17:05 +0300

    Update weather output

--- task2_openweather.py
+++ task2_openweather.py
@@ -4,7 +4,6 @@ import requests
 
 def main():
     api_key = os.getenv("OPENWEATHER_API_KEY")
-    print("API KEY =", api_key)
     city = input("Введите название города: ")
     if not city:
         print("Город не  введен")
@@ -21,5 +20,8 @@ def main():
     data = response.json()
     temp = data["main"]["temp"]
     description = data["weather"][0]["description"]
+    
+    print(f"Температура: {temp:.1f} °C")
+    print(f"Погода: {description}")
 
 main()
